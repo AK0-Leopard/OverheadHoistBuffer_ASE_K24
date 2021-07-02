b0c5adbee84e760ca47f9a235ec878c90532bb20
320 105118 莊義程 <kenzhuang@mirle.com.tw>
2021-07-03 07:56:30 +0800

    新增 Service。 IManualPortService 與 ManualPortService

---
+++ OverheadHoistBuffer_ASE_K24/ScriptControl/Service/ManualPortService.cs
@@ -0,0 +1,16 @@
+﻿using com.mirle.ibg3k0.sc.Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.mirle.ibg3k0.sc.Service
+{
+    public class ManualPortService : IManualPortService
+    {
+        public ManualPortService()
+        {
+        }
+    }
+}
