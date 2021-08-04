eb54d8610ba0d11efedd95edc7498377d760c627
320 105118 莊義程 <kenzhuang@mirle.com.tw>
2021-08-04 13:46:14 +0800

    更新版本至 1.8.0.15

--- OverheadHoistBuffer_ASE_K24/BCWinForm/Properties/AssemblyInfo.cs
+++ OverheadHoistBuffer_ASE_K24/BCWinForm/Properties/AssemblyInfo.cs
@@ -25,12 +25,12 @@ using System.Runtime.InteropServices;
 // 組件的版本資訊是由下列四項值構成:
 //
 //      主要版本
-//      次要版本 
+//      次要版本
 //      組建編號
 //      修訂編號
 //
 // 您可以指定所有的值，也可以依照以下的方式，使用 '*' 將組建和修訂編號
 // 指定為預設值:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.8.0.14")]
-[assembly: AssemblyFileVersion("1.8.0.14")]
+[assembly: AssemblyVersion("1.8.0.15")]
+[assembly: AssemblyFileVersion("1.8.0.15")]
